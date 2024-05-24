afee0e0f50bc263c54cd0dd947eb6f9452b90d67
jguimeram <jguimera54@gmail.com>
2024-05-24 07:15:31 +0200

    updating models

---
+++ src/Models/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Debian\Audiofile\Models;
+
+use PDO;
+
+use Debian\Audiofile\Models\Model;
+use Debian\Audiofile\Routes\Router;
+
+class Product extends Model
+{
+    protected ?int $id;
+    protected string $productname;
+    protected string $description;
+    protected float $price;
+    protected string $stock;
+
+    protected string $dateadded;
+    protected int $categoryid;
+
+    protected static $table = "products";
+    public function __construct($args)
+    {
+        $this->id = $args['id'];
+        $this->productname = $args['productname'];
+        $this->price = $args['price'];
+        $this->stock = $args['stock'];
+        $this->dateadded = $args['dateadded'];
+        $this->categoryid = $args['category'];
+
+
+        foreach ($args as $key => $value) {
+            if ($key === 'id') {
+                continue;
+            }
+            static::$fields[$key] = $value;
+        }
+    }
+}
